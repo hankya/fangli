38898b4afc45706330dffb1d406f47963baf8798
yangrun <yangrun@bytedance.com>
2017-11-18 23:20:22 +0800

    coupon: use varnish
    Change-Id: Ib27762dd60fa415f8c11c71cc76ce70b93a09080

--- coupon/bot.py
+++ coupon/bot.py
@@ -1,13 +1,6 @@
 COUPON_TPL = """已为您找到<a href="{url}">{q}的优惠券</a>"""
 CMD_TPL = """1. 查找优惠券请以"找"开头，例如\"找面膜\""""
-PROXY_URL_TPL = """http://fanyi.baidu.com/transpage?query={target}&from=cht&to=zh&source=url&render=1"""
-TARGET_TPL = """http://taoxmall.com/ProductList.aspx?m=search&a=index&keyWord={q}&ReferralId=8548"""
-
-
-def gen_url(q):
-    target = TARGET_TPL.format(q=q)
-    from urllib.parse import quote_plus
-    return PROXY_URL_TPL.format(target=quote_plus(target))
+TARGET_TPL = """http://119.27.178.166:6081/ProductList.aspx?m=search&a=index&keyWord={q}&ReferralId=8548"""
 
 
 class CouponBot:
@@ -16,6 +9,6 @@ class CouponBot:
         index = msg.find("找")
         if index >= 0:
             q = msg[index + 1:].strip()
-            return COUPON_TPL.format(q=q, url=gen_url(q))
+            return COUPON_TPL.format(q=q, url=TARGET_TPL.format(q=q))
         else:
             return CMD_TPL.format(msg)
