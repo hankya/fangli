df13230459c0da48482491681603ceeee2c8d737
yangrun <yangrun@bytedance.com>
2017-11-18 20:41:13 +0800

    setup loggin
    Change-Id: Ibc0e7312bc19a32e7a883430d524abfd26a54e25

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
+import logging
+
+import sys
+
 import bottle
 from fangli.weixin.app import setup_routing as setup_weixin_routing
 
 app = bottle.Bottle()
 setup_weixin_routing(app)
+
+log_format = "%(asctime)s %(levelname)s %(process)d %(message)s"
+root = logging.getLogger()
+root.handlers = []  # fix reimport issue
+root.setLevel(logging.INFO)
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.INFO)
+ch.setFormatter(log_format)
+root.addHandler(ch)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, accesslog='-', reloader=True)
