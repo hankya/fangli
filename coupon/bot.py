496d69ad38a4ef0cddf757243304c1bec3dc677e
yangrun <yangrun@bytedance.com>
2017-11-18 21:12:31 +0800

    coupon: fix template
    Change-Id: Ibac39c2174a521d7eefe9d9d08b92cc936e4d217

--- coupon/bot.py
+++ coupon/bot.py
@@ -1,4 +1,4 @@
-COUPON_TPL = """已为您找到 *{q}*, 点此前往http://taoxmall.com/ProductList.aspx?m=search&a=index&keyWord={q}&ReferralId=8548"""
+COUPON_TPL = """已为您找到 {q}的优惠券合集 -><a href="http://taoxmall.com/ProductList.aspx?m=search&a=index&keyWord={q}&ReferralId=8548">点击</a>"""
 CMD_TPL = """
 1. 查找优惠券请以"找"开头，例如"找面膜"
 """
@@ -7,7 +7,7 @@ CMD_TPL = """
 class CouponBot:
     @staticmethod
     def respond(msg):
-        index = msg.index("找")
+        index = msg.find("找")
         if index >= 0:
             q = msg[index+1:].strip()
             return COUPON_TPL.format(q=q)
