64136a5d44b2cfa20b3171ad9a6f0a89cb7339c5
yangrun <yangrun@bytedance.com>
2017-11-18 21:37:56 +0800

    coupon:fix broken link
    Change-Id: Ib01ec6fca2a6768fba42a55298061ed2319ad1cc

--- coupon/bot.py
+++ coupon/bot.py
@@ -1,7 +1,13 @@
-COUPON_TPL = """已为您找到 {q}的优惠券合集 -><a href="http://taoxmall.com/ProductList.aspx?m=search&a=index&keyWord={q}&ReferralId=8548">点击</a>"""
-CMD_TPL = """
-1. 查找优惠券请以"找"开头，例如"找面膜"
-"""
+COUPON_TPL = """已为您找到<a href="{url}">{q}的优惠券</a>"""
+CMD_TPL = """1. 查找优惠券请以"找"开头，例如"找面膜"""
+PROXY_URL_TPL = """http://fanyi.baidu.com/transpage?query={target}&from=cht&to=zh&source=url&render=1"""
+TARGET_TPL = """http://taoxmall.com/ProductList.aspx?m=search&a=index&keyWord={q}&ReferralId=8548"""
+
+
+def gen_url(q):
+    from urllib.parse import quote
+    target = TARGET_TPL.format(q=q)
+    return PROXY_URL_TPL.format(target=quote(target))
 
 
 class CouponBot:
@@ -9,7 +15,7 @@ class CouponBot:
     def respond(msg):
         index = msg.find("找")
         if index >= 0:
-            q = msg[index+1:].strip()
-            return COUPON_TPL.format(q=q)
+            q = msg[index + 1:].strip()
+            return COUPON_TPL.format(q=q, url=gen_url(q))
         else:
             return CMD_TPL.format(msg)
