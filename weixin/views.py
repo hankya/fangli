1aed606f4fd4daacfdb3862f1af50b578e671b97
yangrun <yangrun@bytedance.com>
2017-11-19 09:46:19 +0800

    weixin: prettify err
    Change-Id: If9bc1e08b5e470a1c21f37336a782ea5b35bbfb6

--- weixin/views.py
+++ weixin/views.py
@@ -29,9 +29,12 @@ def wx_bot_view():
     signature = request.query.get("signature")
     timestamp = request.query.get("timestamp")
     nonce = request.query.get("nonce")
-    wx_sdk.parse_data(request.body.read(), signature, timestamp, nonce)
+    try:
+        wx_sdk.parse_data(request.body.read(), signature, timestamp, nonce)
+        msg = wx_sdk.message.content
+    except:
+        msg = "服务器打了个盹- -"
     response.status = 200
-    msg = wx_sdk.message.content
     resp = CouponBot.respond(msg)
     wx_resp = wx_sdk.response_text(resp)
     response.status = 200
